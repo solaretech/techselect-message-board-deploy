d6a6e97befb0c40726df83e0e41635a54d07f722
solaretech <solaretech.kk@gmail.com>
2023-03-18 13:40:38 +0900

    Dockerfile.production修正

--- src/db_connect.php
+++ src/db_connect.php
@@ -16,7 +16,7 @@ try {
     $dbUser = getenv('DB_USER');
     $dbPass = getenv('DB_PASS');
   }
-  $dsn = "mysql:dbname=$dbName;host=$dbHost:$dbPort";
+  $dsn = "pgsql:dbname=$dbName;host=$dbHost:$dbPort";
   $dbh = new PDO($dsn, $dbUser, $dbPass);
 } catch (PDOException $e) {
   echo 'データベース接続失敗:';
